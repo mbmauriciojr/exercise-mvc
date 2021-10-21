af396c778a9883da7c8e13309bdc849bbf946a08
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-21 15:23:42 -0300

    feat 4: configurated the server index to know what kind of view are in use, in this case ejs files

--- exercicio-1-revisando/index.js
+++ exercicio-1-revisando/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
