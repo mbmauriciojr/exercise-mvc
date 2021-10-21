181ab2bc3b7039997a04fa1b2f783915a25e7eee
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-21 16:47:01 -0300

    Created the routes for joke and joke/:categorie

--- exercício-2-piadas-chucknorris/index.js
+++ exercício-2-piadas-chucknorris/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const categorieController = require('./controller/categorieController');
+const jokeController = require('./controller/jokeController')
 
 const app = express();
 
@@ -9,6 +11,9 @@ app.set('views', './views');
 const PORT = process.env.PORT || 3000;
 
 app.get('/categories', categorieController.listCategories);
+
+app.get('/jokes', jokeController.listJokes);
+app.get('/jokes/:categorie', jokeController.listJokeByCategorie);
 
 app.listen(PORT, () => {
   console.log(`Ouvindo a porta ${ PORT }`);
